aff7be6cff51bbdff125b020bbecec6f28dd6e27
bearjin <kwj5030@gmail.com>
2021-05-11 17:52:30 +0900

    header 수정 중

--- src/component/layout/header.js
+++ src/component/layout/header.js
@@ -3,7 +3,20 @@ import React, { Component } from 'react';
 class Header extends Component {
     render () {
         return (
-            <header id="header">헤더</header>
+            <header id="header" className="header">
+                <div className="header_inner">
+                    <h1 className="logo"><a href="index.html">logo</a></h1>
+                    <button className="btn-gnb">MENU</button>
+                    <nav id="gnb" className="gnb_wrap">
+                        <ul className="gnb_inner">
+                            <li className="home">HOME</li>
+                            <li className="about">ABOUT ME</li>
+                            <li className="career">CAREER</li>
+                            <li className="portfolio">PORTFOLIO</li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
         );
     }
 }
