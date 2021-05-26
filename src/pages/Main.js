4eb1782443130d73c06d57c505d09c7e2b160448
bearjin <kwj5030@gmail.com>
2021-05-26 16:27:12 +0900

    Sass 설정 변경 및 불필요 파일 삭제

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import HomeContent from '../components/contents/HomeContent';
 import AboutContent from '../components/contents/AboutContent';
 import CareerContent from '../components/contents/CareerContent';
 import ProjectContent from '../components/contents/ProjectContent';
@@ -7,12 +8,7 @@ class Main extends Component {
     render() {
         return (
             <div>
-                <section id="home" className="section">
-                    <div className="sec_inner">
-                        <p>“열정을 가지고 소통하는 웹 퍼블리셔가 되겠습니다.”</p>
-                        <p>Kim Woong Jin</p>
-                    </div>
-                </section>
+                <HomeContent />
                 <AboutContent />
                 <CareerContent />
                 <ProjectContent />
