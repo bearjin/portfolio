76398ea39dd17416f12ed97a58e41aba315b2e2d
bearjin <kwj5030@gmail.com>
2021-07-13 15:10:42 +0900

    포트폴리오 업데이트 - 404 에러 수정중

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Header from './components/structure/Header';
 import Footer from './components/structure/Footer';
 import Main from './pages/Main';
@@ -17,23 +17,25 @@ import Daewoo from './pages/project/Daewoo';
 class App extends Component {
     render() {
         return (
-            <div className="container">
-                <Header />
-                <Switch>
-                    <Route exact path="/" component={Main} />
-                    <Route exact path="/about" component={About} />
-                    <Route exact path="/career" component={Career} />
-                    <Route exact path="/project" component={Project} />
-                    <Route exact path="/project/onlineShop" component={OnlineShop} />
-                    <Route exact path="/project/hanwha" component={Hanwha} />
-                    <Route exact path="/project/onlineSale" component={OnlineSale} />
-                    <Route exact path="/project/kelim" component={Kelim} />
-                    <Route exact path="/project/kyungnam" component={Kyungnam} />
-                    <Route exact path="/project/jaseng" component={Jaseng} />
-                    <Route exact path="/project/daewoo" component={Daewoo} />
-                </Switch>
-                <Footer />
-            </div>
+            <Router>
+                <div className="container">
+                    <Header />
+                    <Switch>
+                        <Route exact path="/" component={Main} />
+                        <Route exact path="/about" component={About} />
+                        <Route exact path="/career" component={Career} />
+                        <Route exact path="/project" component={Project} />
+                        <Route exact path="/project/onlineShop" component={OnlineShop} />
+                        <Route exact path="/project/hanwha" component={Hanwha} />
+                        <Route exact path="/project/onlineSale" component={OnlineSale} />
+                        <Route exact path="/project/kelim" component={Kelim} />
+                        <Route exact path="/project/kyungnam" component={Kyungnam} />
+                        <Route exact path="/project/jaseng" component={Jaseng} />
+                        <Route exact path="/project/daewoo" component={Daewoo} />
+                    </Switch>
+                    <Footer />
+                </div>
+            </Router>
         );
     }
 }
