eebfc729ae374c914771a9ba88e016c02c831762
bearjin <kwj5030@gmail.com>
2021-07-14 15:19:34 +0900

    어바웃 페이지 수정

--- src/components/contents/AboutContent.js
+++ src/components/contents/AboutContent.js
@@ -1,180 +1,80 @@
 import React, { Component } from 'react';
+import { AboutIntroData, AboutSkillData } from '../../data/AboutData';
 
 class AboutContent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            IntroData: AboutIntroData,
+            SkillData: AboutSkillData
+        }
+    }
+
+    makeIntroLink() {
+        const links = this.state.IntroData.links.map((elm, idx) => {
+            return (
+                <a key={idx} className="link" href={elm.link} target="_blank" rel="noreferrer" title="새 창 열림">elm.name</a>
+            );
+        });
+        return links;
+    }
+
+    makeSkillArea() {
+        const skills = this.state.SkillData.map((elm, idx) => {
+            return (
+                <div key={idx} className="skills">
+                    <h3 className="subj">{elm.subj}</h3>
+                    <ul className="skill_list">
+                        {this.makeSkillList(idx)}
+                    </ul>
+                </div>
+            );
+        });
+        return skills;
+    }
+
+    makeSkillList(areaIdx) {
+        const lists = this.state.SkillData[areaIdx].skills.map((elm, idx) => {
+            return (
+                <li key={idx} className="item" data-item={elm.data}>
+                    <p className="name">{elm.name}</p>
+                    <div className="info">
+                        <div className="img"><img src={require(`../../resources/images/about/${elm.img}.png`).default} alt="img" /></div>
+                        <div className="rank">{this.makeRankMeter(elm.rank)}</div>
+                    </div>
+                </li>
+            );
+        });
+        return lists;
+    }
+
+    makeRankMeter(rankNum) {
+        let ranks = [];
+        for (let index = 0; index < 10; index++) {
+            if (index < rankNum) {
+                ranks.push(<span key={index} className="rank_meter active"></span>);
+            } else {
+                ranks.push(<span key={index} className="rank_meter"></span>);
+            }
+        }
+        return ranks;
+    }
+
     render() {
         return (
             <section id="aboutCont" className="sec about">
                 <div className="sec_inner">
                     <h2 className="sec_subj">ABOUT</h2>
                     <div className="about_intro">
-                        <p className="name">Bearjin</p>
-                        <p className="txt">
-                            약 4년간 웹 에이전시에서 퍼블리셔로써 다양한 기업사이트 제작과 유지운영 업무를 수행했습니다.
-                            무던하고 책임감 강한 성격으로 타팀과의 협업에서 트러블 없이 프로젝트들을 성공적으로 오픈했고, 항상 좋은 평가를 받아 왔습니다.
-                            최근에는 온라인 구매 예약 서비스를 제작하며 데이터를 다루는데 흥미를 느껴 Frontend Developer를 목표로 역량을 키워 나가고 있습니다.
-                        </p>
-                        <a className="email" href="mailto:kwj5030@gmail.com" target="_blank" rel="noreferrer" title="새 창 열림">kwj5030@gmail.com</a>
+                        <p className="name">{this.state.IntroData.name}</p>
+                        <p className="txt">{this.state.IntroData.txt}</p>
+                        <a className="email" href={`mailto:${this.state.IntroData.email}`} target="_blank" rel="noreferrer" title="새 창 열림">{this.state.IntroData.email}</a>
                         <ul className="links">
-                            <a className="link" href="https://github.com/bearjin" target="_blank" rel="noreferrer" title="새 창 열림">Github</a>
-                            <a className="link" href="https://bearjin.github.io" target="_blank" rel="noreferrer" title="새 창 열림">Blog</a>
-                            <a className="link" href="https://bearjin.github.io/portfolio">Portfolio</a>
+                            {this.makeIntroLink()}
                         </ul>
                     </div>
                     <div className="about_skill">
-                        <div className="skills">
-                            <h3 className="subj">Programming Skill</h3>
-                            <ul className="skill_list">
-                                <li className="item" data-item="html">
-                                    <p className="name">HTML</p>
-                                    <div className="info">
-                                        <div className="img"><img src={require('../../resources/images/about/html5.png').default} alt="img" /></div>
-                                        <div className="rank">
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter"></span>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li className="item" data-item="css">
-                                    <p className="name">CSS</p>
-                                    <div className="info">
-                                        <div className="img"><img src={require('../../resources/images/about/css3.png').default} alt="img" /></div>
-                                        <div className="rank">
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter"></span>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li className="item" data-item="sass">
-                                    <p className="name">Sass</p>
-                                    <div className="info">
-                                        <div className="img"><img src={require('../../resources/images/about/sass.png').default} alt="img" /></div>
-                                        <div className="rank">
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter"></span>
-                                            <span className="rank_meter"></span>
-                                            <span className="rank_meter"></span>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li className="item" data-item="js">
-                                    <p className="name">Javascript</p>
-                                    <div className="info">
-                                        <div className="img"><img src={require('../../resources/images/about/js.png').default} alt="img" /></div>
-                                        <div className="rank">
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter"></span>
-                                            <span className="rank_meter"></span>
-                                            <span className="rank_meter"></span>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li className="item" data-item="jquery">
-                                    <p className="name">jQuery</p>
-                                    <div className="info">
-                                        <div className="img"><img src={require('../../resources/images/about/jquery.png').default} alt="img" /></div>
-                                        <div className="rank">
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter"></span>
-                                            <span className="rank_meter"></span>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li className="item" data-item="react">
-                                    <p className="name">React.js</p>
-                                    <div className="info">
-                                        <div className="img"><img src={require('../../resources/images/about/react.png').default} alt="img" /></div>
-                                        <div className="rank">
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter"></span>
-                                            <span className="rank_meter"></span>
-                                            <span className="rank_meter"></span>
-                                            <span className="rank_meter"></span>
-                                            <span className="rank_meter"></span>
-                                        </div>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-                        <div className="skills">
-                            <h3 className="subj">Tools</h3>
-                            <ul className="skill_list">
-                                <li className="item" data-item="git">
-                                    <p className="name">Git</p>
-                                    <div className="info">
-                                        <div className="img"><img src={require('../../resources/images/about/git.png').default} alt="img" /></div>
-                                        <div className="rank">
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter"></span>
-                                            <span className="rank_meter"></span>
-                                            <span className="rank_meter"></span>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li className="item" data-item="photoshop">
-                                    <p className="name">Photoshop</p>
-                                    <div className="info">
-                                        <div className="img"><img src={require('../../resources/images/about/photoshop.png').default} alt="img" /></div>
-                                        <div className="rank">
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter active"></span>
-                                            <span className="rank_meter"></span>
-                                            <span className="rank_meter"></span>
-                                            <span className="rank_meter"></span>
-                                            <span className="rank_meter"></span>
-                                        </div>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
+                        {this.makeSkillArea()}
                     </div>
                 </div>
             </section>
---
+++ src/data/AboutData.js
@@ -0,0 +1,92 @@
+const AboutIntroData = {
+    name: 'Bearjin',
+    txt: `
+    약 4년간 웹 에이전시에서 퍼블리셔로써 다양한 기업사이트 제작과 유지운영 업무를 수행했습니다.
+    무던하고 책임감 강한 성격으로 타팀과의 협업에서 트러블 없이 프로젝트들을 성공적으로 오픈했고, 항상 좋은 평가를 받아 왔습니다.
+    최근에는 온라인 구매 예약 서비스를 제작하며 데이터를 다루는데 흥미를 느껴 Frontend Developer를 목표로 역량을 키워 나가고 있습니다.
+    `,
+    email: 'kwj5030@gmail.com',
+    links: [
+        {
+            name: 'Github',
+            link: 'https://github.com/bearjin'
+        },
+        {
+            name: 'Blog',
+            link: 'https://bearjin.github.io'
+        },
+        {
+            name: 'Portfolio',
+            link: 'https://bearjin.github.io/portfolio'
+        }
+    ]
+}
+
+const AboutSkillData = [
+    {
+        subj: 'Programming Skill',
+        skills: [
+            {
+                data: 'html',
+                name: 'HTML',
+                img: 'html5',
+                rank: 9
+            },
+            {
+                data: 'css',
+                name: 'CSS',
+                img: 'css3',
+                rank: 9
+            },
+            {
+                data: 'sass',
+                name: 'SCSS',
+                img: 'sass',
+                rank: 7
+            },
+            {
+                data: 'js',
+                name: 'Javascript',
+                img: 'js',
+                rank: 7
+            },
+            {
+                data: 'jquery',
+                name: 'jQuery',
+                img: 'jquery',
+                rank: 8
+            },
+            {
+                data: 'react',
+                name: 'React.js',
+                img: 'react',
+                rank: 5
+            },
+        ]
+    },
+    {
+        subj: 'Tools',
+        skills: [
+            {
+                data: 'git',
+                name: 'Git',
+                img: 'git',
+                rank: 7
+            },
+            {
+                data: 'sourcetree',
+                name: 'Sourcetree',
+                img: 'sourcetree',
+                rank: 8
+            },
+            {
+                data: 'photoshop',
+                name: 'Photoshop',
+                img: 'photoshop',
+                rank: 6
+            },
+        ]
+    },
+]
+
+export { AboutIntroData, AboutSkillData };
