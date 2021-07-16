d6a5dd3a5c54d9a1e149d9007cfa624a516407e8
bearjin <kwj5030@gmail.com>
2021-07-16 20:00:11 +0900

    도메인 변경으로 링크 변경

--- src/data/AboutData.js
+++ src/data/AboutData.js
@@ -1,9 +1,11 @@
 const AboutIntroData = {
     name: 'Bearjin',
     txt: `
-    약 4년간 웹 에이전시에서 퍼블리셔로써 다양한 기업사이트 제작과 유지운영 업무를 수행했습니다.
-    무던하고 책임감 강한 성격으로 타팀과의 협업에서 트러블 없이 프로젝트들을 성공적으로 오픈했고, 항상 좋은 평가를 받아 왔습니다.
-    최근에는 온라인 구매 예약 서비스를 제작하며 데이터를 다루는데 흥미를 느껴 Frontend Developer를 목표로 역량을 키워 나가고 있습니다.
+    안녕하세요! 4년차 웹개발자 김웅진입니다. 
+    그동안 다양한 기업 사이트 제작과 유지운영 업무를 수행했습니다. 
+    무던하고 책임감 강한 성격으로 타팀과의 협업에서 트러블 없이 프로젝트들을 일정에 맞춰 성공적으로 오픈했고, 좋은 평가를 받아 왔습니다. 
+    최근에는 온라인 샵 서비스를 통해 한 페이지안에서 데이터와 사용자의 선택에 따라 내용들이 변경되는 서비스를 구현하며 데이터 관리와 SPA(single page application)을 좀 더 쉽게 구현하기 위한 React의 필요성을 느꼈습니다. 
+    현재는 React를 학습하며 Front-end developer로써 성장해 나가고 있습니다.
     `,
     email: 'kwj5030@gmail.com',
     links: [
@@ -13,11 +15,11 @@ const AboutIntroData = {
         },
         {
             name: 'Blog',
-            link: 'https://bearjin.github.io'
+            link: 'https://bearjin.com'
         },
         {
             name: 'Portfolio',
-            link: 'https://bearjin.github.io/portfolio'
+            link: 'https://bearjin.com/portfolio'
         }
     ]
 }
