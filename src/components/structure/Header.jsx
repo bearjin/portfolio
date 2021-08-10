ab37957ac49eaf82c826b503f946a9a0e3ac9cb6
bearjin <kwj5030@gmail.com>
2021-08-10 16:58:34 +0900

    Update: header css

--- src/components/structure/Header.jsx
+++ src/components/structure/Header.jsx
@@ -9,18 +9,18 @@ const Header = () => {
 
   return (
     <header id="header" className="header">
-      <div className="header-inner">
-        <h1 className="header-logo"><Link className="header-logo__link" to="/portfolio">Home</Link></h1>
-        <button className="header-menu" onClick={onClickMenu}>
-          <span className="header-menu__bar"></span>
-          <span className="header-menu__text">Menu</span>
+      <div className="header__inner">
+        <h1 className="header__logo"><Link className="header__logo-link" to="/portfolio">Home</Link></h1>
+        <button className="header__menu" onClick={onClickMenu}>
+          <span className="header__menu-bar"></span>
+          <span className="header__menu-text">Menu</span>
         </button>
         <nav id="gnb" className="gnb">
-          <ul className="gnb-inner">
-            <li className="gnb-inner__item"><Link className="gnb-inner__link" to="/portfolio/about">About</Link></li>
-            <li className="gnb-inner__item"><Link className="gnb-inner__link" to="/portfolio/project">Project</Link></li>
-            <li className="gnb-inner__item"><Link className="gnb-inner__link" to="/portfolio/career">Career</Link></li>
-            <li className="gnb-inner__item"><a className="gnb-inner__link" href="https://bearjin.github.io/" target="_blank" rel="noreferrer" title="새 창 열림">Blog</a></li>
+          <ul className="gnb__inner">
+            <li className="gnb__inner-item"><Link className="gnb__inner-link" to="/portfolio/about">About</Link></li>
+            <li className="gnb__inner-item"><Link className="gnb__inner-link" to="/portfolio/project">Project</Link></li>
+            <li className="gnb__inner-item"><Link className="gnb__inner-link" to="/portfolio/career">Career</Link></li>
+            <li className="gnb__inner-item"><a className="gnb__inner-link" href="https://bearjin.github.io/" target="_blank" rel="noreferrer" title="새 창 열림">Blog</a></li>
           </ul>
         </nav>
       </div>
